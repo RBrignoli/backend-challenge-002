f244e6e38b6452a03f16e0c213772ae3c5eb568c
Ramon Brignoli <ramon@Ramons-MacBook-Pro.local>
2020-11-09 10:55:22 -0300

    adicionando challenge models

--- challenges/models.py
+++ challenges/models.py
@@ -5,10 +5,43 @@ Challenges Models
 # Libraries
 ###
 from django.db import models
-
+from helpers.models import TimestampModel
 ###
 # Choices
 ###
+TRUST_MORE_STRESS_LESS = 'trust_more_and_stress_less'
+HAVE_REFRESHING_REST = 'have_refreshing_rest'
+ENJOY_SUNLIGHT = 'enjoy_sunlight'
+NUTRITION = 'nutrition'
+EXERCISE = 'exercise'
+WATER = 'water'
+LIVE_TEMPERATELY = 'live_temperately'
+INVEST_IN_OTHERS = 'invest_in_others'
+FRESH_AIR = 'fresh_air'
+EDUCATION = 'education'
+SUPER_CHALLENGE = 'super_challenge'
+
+CHALLENGE_TYPE_CHOICES = [
+    TRUST_MORE_STRESS_LESS,
+    HAVE_REFRESHING_REST,
+    ENJOY_SUNLIGHT,
+    NUTRITION,
+    EXERCISE,
+    WATER,
+    LIVE_TEMPERATELY,
+    INVEST_IN_OTHERS,
+    FRESH_AIR,
+    EDUCATION
+    ]
+
+DEFAULT_SCORE = 1,
+SUPER_SCORE = 10,
+
+SCORE_CHOICES = [
+    DEFAULT_SCORE,
+    SUPER_SCORE,
+    ]
+
 
 
 ###
@@ -20,8 +53,63 @@ from django.db import models
 # Models
 ###
 
-TYPE_CHOICES = [
-        (MALE, _(MALE)),
-        (FEMALE, _(FEMALE)),
-        (OTHER, _(OTHER)),
+TRUST_MORE_STRESS_LESS = 'trust_more_and_stress_less'
+HAVE_REFRESHING_REST = 'have_refreshing_rest'
+ENJOY_SUNLIGHT = 'enjoy_sunlight'
+NUTRITION = 'nutrition'
+EXERCISE = 'exercise'
+WATER = 'water'
+LIVE_TEMPERATELY = 'live_temperately'
+INVEST_IN_OTHERS = 'invest_in_others'
+FRESH_AIR = 'fresh_air'
+EDUCATION = 'education'
+SUPER_CHALLENGE = 'super_challenge'
+
+CHALLENGE_TYPE_CHOICES = [
+    TRUST_MORE_STRESS_LESS,
+    HAVE_REFRESHING_REST,
+    ENJOY_SUNLIGHT,
+    NUTRITION,
+    EXERCISE,
+    WATER,
+    LIVE_TEMPERATELY,
+    INVEST_IN_OTHERS,
+    FRESH_AIR,
+    EDUCATION
     ]
+
+class Challenges(TimestampModel):
+    category = models.CharField(
+        choices=CHALLENGE_TYPE_CHOICES,
+        verbose_name=('category'),
+        max_length=32,
+    )
+    title = models.CharField(
+        verbose_name=('title'),
+        max_length=256,
+        null=True,
+        blank = True,
+    )
+    text = models.TextField(
+        verbose_name=('text'),
+        null = True,
+        blank = True,
+    )
+
+    is_super = models.BooleanField(
+        verbose_name=_('is super'),
+        default= False,
+        blank = True,
+    )
+    score = models.PositiveSmallIntegerField(
+        verbose_name=('score'),
+        default = DEFAULT_SCORE,
+        blank = True,
+    )
+    URLName = models.SlugField(
+        max_length=255,
+        unique=True
+    )
+
+    def __str__(self):
+        return self.title
